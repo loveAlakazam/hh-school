b0f9dc02a3d57a93cb201201c9b714ed73fc3c12
eunkang-choi <dmsrkd1216@gmail.com>
2024-12-22 11:27:44 +0900

    init: 프로젝트 초기생성

---
+++ src/main/java/io/hhpulus/school/HHPlusSchoolMainApplication.java
@@ -0,0 +1,11 @@
+package io.hhpulus.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HHPlusSchoolMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HHPlusSchoolMainApplication.class, args);
+    }
+}

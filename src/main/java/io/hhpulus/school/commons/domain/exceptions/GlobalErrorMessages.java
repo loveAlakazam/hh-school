33f42a0032f5cc59003f480aaa6e18a044992b73
eunkang-choi <dmsrkd1216@gmail.com>
2024-12-29 09:46:42 +0900

    update: 공용리소스 & 공용에러 수정
    - GlobalException 에러 핸들러 추가 (에러코드 500 로 핸들링)
    - IllegalArgumentException 에러 핸들러 추가 (에러코드 400로 핸들링)
    - Lombok 셋팅

---
+++ src/main/java/io/hhpulus/school/commons/domain/exceptions/GlobalErrorMessages.java
@@ -0,0 +1,6 @@
+package io.hhpulus.school.commons.domain.exceptions;
+
+public interface GlobalErrorMessages {
+    String INTERNAL_SERVER_ERROR_MESSAGE = "서버 내부에서 발생한 오류입니다.";
+    String BAD_REQUEST_ERROR_MESSAGE = "잘못된 요청값 입니다.";
+}
